c96369d99596ea806505098e5b3a82bac462790a
Bulelani <bkani2304@gmail.com>
2025-08-13 21:07:47 +0200

    create UI for Header

---
+++ my-todo/src/components/Header.jsx
@@ -0,0 +1,22 @@
+
+const Header = ()=>{
+
+     return(<>
+
+          <header>
+               <div className="app-tittle">
+                    My To Do Program
+               </div>
+               <div className="links-section">
+                    <a href="#">About Us</a>
+                    <a href="#">Contacts</a>
+                    <a href="#">More Information</a>
+               </div>
+          </header>
+     
+     </>)
+     
+
+}
+
+export default Header;
